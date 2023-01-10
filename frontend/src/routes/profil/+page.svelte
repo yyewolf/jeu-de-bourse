c90a61fe83a7447e76618ae94dc5c0b2f7709b3a
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2023-01-10 10:03:18 +0100

    [+] ajout des articles

--- frontend/src/routes/profil/+page.svelte
+++ frontend/src/routes/profil/+page.svelte
@@ -12,4 +12,24 @@
         });
 </script>
 
-<span>{JSON.stringify(infos)}</span>
+<div class="flex flex-col max-w-full text-white mt-5">
+    <div class="flex flex-col items-center">
+        <div class="items-center">
+            <p class="text-2xl">Profil de <span class="font-extrabold">{infos.username}</span></p>
+            <div class="divider"></div> 
+        </div>
+        <div class="items-center">
+            <p>Valorisation : {(infos.valorisation/10000).toFixed(2)}€ 
+                {#if infos.var_valorisation > 0}
+                    <span class="text-green-500">(+{(infos.var_valorisation/10000).toFixed(2)}%=</span>
+                {:else if infos.var_valorisation < 0}
+                    <span class="text-red-500">(-{(infos.var_valorisation/10000).toFixed(2)}%)</span>
+                {:else}
+                    <span class="text-white">(+0.00%)</span>
+                {/if}
+            </p> 
+            <p>Disponible au comptant : {(infos.comptant/10000).toFixed(2)}€</p>  
+            <p>Disponible au SRD : {(infos.srd/10000).toFixed(2)}€</p>     
+        </div>
+    </div>
+</div>
